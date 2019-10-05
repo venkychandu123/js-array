b0375d5b150c8e7fa15a8cb793fe7fbeddbfcc51
naresh <na1432na1432@gmail.com>
2019-10-05 16:11:31 +0530

    arrays

---
+++ 1.js
@@ -0,0 +1,59 @@
+ //   ------ ARRAYS---------
+//  var array=['anu','naru','amma','nanna'];
+//  console.log(array.length);
+
+
+
+ //---case1-----
+//  var array1=['anu','naru','amma','nanna'];
+//  console.log(array.length);
+//  console.log(array[2]);
+                                              //-------case2-----
+//  var arrat2=new Array('anu','naresh');
+//  console.log(arrat2);
+
+
+// -------------------------------------------csse3-----------------------------
+// var array =Array(8);
+
+// console.log(array);
+
+//-----------------------------------------------case4----------------------
+// var array=Array.of('anu','naresh');
+
+// console.log(array);
+
+
+
+
+// console.log(Array.isArray(array));
+
+//--------string into array--type1------------------------------
+
+// var str="hi node js ";
+// var str1=str.split('');//if you give no ''space split into individual word//if you give' '  one space split into word
+//if you give'  '  two space split into entire word
+// console.log(str1);
+
+
+
+
+
+//--------string into array--type2-----1-----es-6--------------------
+
+
+// var st="hi anu how are you what about you";
+// var st1=Array.from(st);
+// console.log(st1);
+
+
+//--------string into array--type3------2----es-6--------------------
+
+
+// var st="hi anun how are you";
+// var [...anu]=st;
+// console.log(anu.join(''));
+
+
+
+
