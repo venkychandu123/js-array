542456bbb397ea97a46cd2120921fc0de4b8a2f9
naresh <na1432na1432@gmail.com>
2019-10-09 14:13:43 +0530

    my file

--- 1.js
+++ 1.js
@@ -1,7 +1,8 @@
- //   ------ ARRAYS---------
+ //  ------js non-primitive-data-types------
+ 
+ //------ ARRAYS---------
 //  var array=['anu','naru','amma','nanna'];
 //  console.log(array.length);
-
 
 
  //---case1-----
@@ -13,7 +14,7 @@
 //  console.log(arrat2);
 
 
-// -------------------------------------------csse3-----------------------------
+// -------------------------------------------csse3--------------------------------------------
 // var array =Array(8);
 
 // console.log(array);
@@ -53,6 +54,175 @@
 // var st="hi anun how are you";
 // var [...anu]=st;
 // console.log(anu.join(''));
+
+
+
+
+
+//-------------------------------------------object------  type1-------------------------------------------
+
+// var obj={
+//     //key:value based
+//     name:"naresh",
+//     age:21,
+//     company:"xyz",
+//     salary:000000
+// };
+// console.log(obj);
+
+//------------------------------------------------type2------------------------
+
+
+// var obj =new Object();
+// .name="anu";
+// obj.age=21;obj
+// obj.company="xyz";
+// obj.sal=000;
+// console.log(obj);
+
+
+
+
+// var name="naresh";
+// var age=21;
+// var company="xyz";
+// var obj ={
+//     name,
+//     age,
+//     company,
+//     sal:100000
+// };
+// console.log(obj);
+
+
+
+//---------------------------------multiple object declaration---------------------------------------
+
+
+// var obj=[{name:"naresh",
+//     age:21,    company:"xyz",
+//   salary:000000},{name:"naresh",
+//       age:21,
+//        company:"xyz",
+//       salary:000000}];
+//       console.log(obj);
+
+
+
+
+
+
+
+//-------------if we can easly change the object data---------------
+// var obj ={
+//     name:"naresh",
+//     age:21,
+//     company:"xyz",
+//      salary:000000
+// };
+// obj.name="anupama";
+// console.log(obj);
+
+//-----------------------------by using exising obj structure we can easily adding the new user data
+
+// var obj={name:"naresh",
+//      age:21,
+//      company:"xyz",
+//    salary:000000
+// };
+// var obj1=Object.create(obj);
+// obj1.name="naresh";
+// obj1.age=21;
+// obj1.sal=1000;
+// console.log(obj1);
+
+//----------------------------entries methode --by using this we can return data in the form of array-----------
+// var obj={
+//     name:"naresh",
+//     age:21,
+//     company:"xyz",
+//     salary:000000
+// };
+// console.log(Object.entries(obj));
+// console.log(Object.values(obj));
+// console.log(Object.keys(obj)); 
+// var hhh=Object.freeze(obj);
+// obj.name="anu";
+
+
+
+//-----------------------functions----------------------------------------------------------------------------------------------
+//function synt
+
+
+
+//--------anonymous function
+
+
+// function()
+// {
+//     alert('hello potti');
+// } 
+
+//--------function expression
+
+
+//    var user=function()
+//     {
+//     alert('hello potti');
+//      } 
+//        user();
+//  (function(){
+//      alert('hello');
+//      console.log('hi potti');
+//      var username="anu";
+//      var password=1432;
+
+//  })();
+//  console.log(username,password);//error-bcoz that two available with in the scope------------------------
+
+//---------------------------es-6 syntax for iife
+//     {
+//      alert('hello');
+//      console.log('hi potti');
+//  var username="anu"; 
+//     var password=1432;
+// }
+
+
+
+//-------------------naming function-------------------------
+
+// function  user(){
+//     console.log('hi bangaram');
+//     console.log('anu');
+//     var username="anusha";
+//     var password=123;
+
+// }
+// user()//calling function-----
+
+
+//-------------------function con----
+
+ 
+
+// var user=new Function(console.log('anu'));
+// user();
+
+
+
+
+
+
+
+// -----function with parameter-----------------------------------------------------
+// function user(user){
+//     return user; //-------------- return statement always optional
+// }
+// console.log(user('anu'));
+// console.log(user('an'));
+// console.log(user('na'));
 
 
 
